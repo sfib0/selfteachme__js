e988ac316d2f00d245609d2fa72273272700ba9f
SaadFarhanIdress <saadfarhan347@gmail.com>
2022-08-12 02:30:52 +0500

    Add 2 alert()

---
+++ 2-alert/script.js
@@ -0,0 +1,9 @@
+/*
+  alert() is a built-in javascript function which can be
+  used to display an alert message to the screen.
+*/
+
+alert("It's an alert!")
+
+// You can also use it like that:
+window.alert("It's an alert!")
