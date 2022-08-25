33b23727eba3b432d51ae4d2b54e4d03a41d3914
SaadFarhanIdress <saadfarhan347@gmail.com>
2022-08-25 12:02:49 +0500

    Add 8-case-sensitive-&-camel-case

---
+++ 8-case-sensitive-&-camel-case/index.js
@@ -0,0 +1,29 @@
+/*
+  # Comments
+  - Comments are that part of the code which is not going to be executed.
+  - Programmer often need to commend some code.
+  - Comments may used to explain the code but its not recommended for this purpose.
+  - You may need to comment some code for testing and debugging purpose.
+  - There are two types of comments:
+    - Single line comment
+    - Multi line comment
+*/
+
+// 1. Single line comment
+
+// The following line will not be executed.
+// console.log('Hello World');
+
+/*
+  2. Multi line comment
+*/
+
+// The following lines will not be executed.
+/*
+  var a = 1;
+  var b = 2;
+  var c = a + b;
+  console.log(c);
+*/
+
+
