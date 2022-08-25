3eba1371e836b5074c1dc43564bac41caa10ceb0
SaadFarhanIdress <saadfarhan347@gmail.com>
2022-08-25 12:02:23 +0500

    Add 6-expressions

---
+++ 6-expressions/index.js
@@ -0,0 +1,15 @@
+/*
+  # Expressions
+  - An expression is a combination of values, variables, and operators which
+  computes a value.
+  - Anything which will be evaluated to a value is an expression.
+  - The computation is called an evaluation.
+*/
+
+// For example:
+a = 1; 
+b = 2;
+a + b; // expression
+4 / 2; // expression
+"Hello" + "World"; // expression
+
