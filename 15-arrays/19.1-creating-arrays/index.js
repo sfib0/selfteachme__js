5acd3065c80937979aef94a4b6e61baeb6ca5edc
SaadFarhanIdress <saadfarhan347@gmail.com>
2022-09-05 10:41:52 +0500

    feat(lesson): add 15-arrays

---
+++ 15-arrays/19.1-creating-arrays/index.js
@@ -0,0 +1,84 @@
+// Maybe you may need to work with more than
+// 10 names of US Presidents. In this case,
+
+const FirstPresident = "George Washington";
+const SecondPresident = "John Adams";
+const ThirdPresident = "Thomas Jefferson";
+const FourthPresident = "James Madison";
+const FifthPresident = "James Monroe";
+const SixthPresident = "John Quincy Adams";
+const SeventhPresident = "Andrew Jackson";
+const EighthPresident = "Martin Van Buren";
+const NinthPresident = "William Henry Harrison";
+const TenthPresident = "John Tyler";
+
+// Now imagine, working with 10 different variables.
+// Like if we want to print every president's name
+// to the console, we would have to write 10 console.log
+// statements.
+
+// An alternative to this is to use an array.
+const TenUSPresidents = [
+  "George Washington",
+  "John Adams",
+  "Thomas Jefferson",
+  "James Madison",
+  "James Monroe",
+  "John Quincy Adams",
+  "Andrew Jackson",
+  "Martin Van Buren",
+  "William Henry Harrison",
+  "John Tyler"
+];
+
+// - An array can have multiple values of different
+// data types. In the above example, we have an 
+// array named TenUSPresidents which has 10 values.
+
+// - You can access the values of an array by using
+// the index of the value. The index of an array
+// starts from 0. So the first value of an array
+// will have an index of 0, the second value will
+// have an index of 1, and so on.
+
+// To access first index of the TenUSPresidents array:
+console.log('First US president: ' + TenUSPresidents[0] + '(1789-1797)');
+
+// Now access the last index of the TenUSPresidents array:
+console.log('Last US president: ' + TenUSPresidents[9] + '(1841-1845)');
+
+// - We can mix different data types in an array.
+const MixedArray = [
+  "Hello",
+  1,
+  true,
+  2.5,
+  false,
+  "World"
+];
+
+// MixedArray has string value on index 0 and index
+// 5,
+// number value on index 1 and index 3,
+// and boolean value on index 2 and index 4.
+
+// - To assign a value to an array, we can first 
+// type the array name, then the index of the
+// value we want to assign, then the value we
+// want to assign. If we forget to type the index
+// of the value we want to assign, then the array
+// will become a normal variable.
+
+// Wrong way:
+// TenUSPresidents = "James K. Polk";
+
+// Right way:
+TenUSPresidents[10] = "James K. Polk";
+// TenUSPresidents now has 11 values.
+
+// - Its convention to use plural names for arrays.
+// For example, TenUSPresidents is a better name
+// than TenUSPresident.
+
+// - Same naming rules also apply to arrays.
+
---
+++ 15-arrays/19.3-removing-insertings-extracting-elements/index.js
@@ -0,0 +1,82 @@
+// Let's define an empty array:
+const pets = [];
+
+// Now let's add some values to the array:
+pets[0] = "Dog";
+pets[1] = "Cat";
+pets[2] = "Fish";
+pets[3] = "Lion";
+pets[4] = "Tiger";
+pets[5] = "Bear";
+
+// # unshift method
+
+// Now if we want to add elements to the beginning
+// of the array, we can use the `unshift` method:
+
+pets.unshift("Bird", "Rabbit");
+
+// # splice method
+
+// If we want to add elements anywhere as well
+// as remove some elements that conflicts with
+// the specified index, we can use the `splice`
+// method:
+
+// ## Adding as well as removing elements
+
+pets.splice(2, 2, "Hamster", "Turtle", "Dinosaur");
+
+// In the above code, the first argument is the
+// index where we want to start adding or removing
+// elements. The second argument is the number of
+// elements we want to remove. And all the other
+// arguments are the elements we want to add.
+
+// Now the `pets` array will be:
+console.log(pets); // ["Bird", "Rabbit", "Hamster", "Turtle", "Dinosaur", "Lion", "Tiger", "Bear"]
+
+// ## Only adding elements
+
+// We could add elements without removing existing
+// elements by passing 0 as the second argument:
+pets.splice(2, 0, "Fish", "Lion");
+
+// Now the `pets` array will be:
+console.log(pets); // ["Bird", "Rabbit", "Fish", "Lion", "Hamster", "Turtle", "Dinosaur", "Lion", "Tiger", "Bear"]
+
+// ## Only removing elements
+
+// We could remove elements without adding new
+// elements by not passing the third argument:
+pets.splice(2, 2);
+
+// Now the `pets` array will be:
+console.log(pets); // ["Bird", "Rabbit", "Hamster", "Turtle", "Dinosaur", "Lion", "Tiger", "Bear"]
+
+// # slice method
+
+// - If we want to extract elements from an array,
+// we can use the `slice` method. The following
+// code will extract the elements from index 2
+// to index 4 and store them in a new array called
+// `twoPets` leaving the original `pets` array
+// unchanged:
+const twoPets = pets.slice(2, 4);
+console.log(twoPets); // ["Hamster", "Turtle"]
+
+// - The first argument of the `slice` method is the index
+// where we want to start extracting elements.
+// - The second argument is the index before which we
+// want to stop extracting elements which means
+// that in the above code, the index 2 and 3 gets
+// copies to a new `twoPets` `const`.
+
+// - If we don't pass the second argument, it will extract all
+// the elements from the first argument index to
+// the end of the array.
+
+// - If you did not assign the extracted elements
+// to a new variable, the `slice` method will
+// reduce the original array to the extracted
+// elements.
